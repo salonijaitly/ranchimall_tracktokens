8871c1d73e56e7e0d6ddb6b9dea2bfa234ba2756
Saloni Jaitly <59107893+salonijaitly@users.noreply.github.com>
2021-09-16 14:21:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+committeeAddressList = ['oVwmQnQGtXjRpP7dxJeiRGd5azCrJiB6Ka']
+
+sseAPI_url = 'https://ranchimallflo-testnet.duckdns.org/'
+
+privKey = 'RG6Dni1fLqeT2TEFbe7RB9tuw53bDPDXp8L4KuvmYkd5JGBam6KJ'
